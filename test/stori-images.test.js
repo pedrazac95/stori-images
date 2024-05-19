2f97c701bfa7323c3fe05a939571906ab58859dd
pedrazac95 <ca.pedraza10@gmail.com>
2024-05-19 13:58:38 -0500

    cdk app created

---
+++ test/stori-images.test.js
@@ -0,0 +1,17 @@
+// const cdk = require('aws-cdk-lib');
+// const { Template } = require('aws-cdk-lib/assertions');
+// const StoriImages = require('../lib/stori-images-stack');
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/stori-images-stack.js
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//   // WHEN
+//   const stack = new StoriImages.StoriImagesStack(app, 'MyTestStack');
+//   // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
